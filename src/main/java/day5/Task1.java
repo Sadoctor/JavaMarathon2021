38d5c9e70a7fdd15fffb10709d5520a8d2ee15a4
Boris Goncharuk <boris_goncharuk99@mail.ru>
2023-09-02 00:44:56 +0400

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,42 @@
 package day5;
 
+
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setModel("Audi");
+        car1.setColor("White");
+        car1.setYear_of_issue(2020);
 
+        System.out.println(car1.getModel());
+        System.out.println(car1.getColor());
+        System.out.println(car1.getYear_of_issue());
+    }
+}
+
+class Car{
+
+    private String model;
+    private String color;
+    private int year_of_issue;
+
+    public void setModel(String usermodel){
+        model = usermodel;
+    }
+    public void setColor(String usercolor) {
+        color = usercolor;
+    }
+    public void setYear_of_issue(int user_year_of_issue) {
+        year_of_issue = user_year_of_issue;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYear_of_issue() {
+        return year_of_issue;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,37 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike bike1 = new Motorbike("Yamaha", "Black", 2010);
+        bike1.getbike();
 
+    }
+}
+
+class Motorbike{
+
+    private String model;
+    private String color;
+    private int year_of_issue;
+
+    public Motorbike(String model, String color, int year_of_issue) {
+        this.model = model;
+        this.color = color;
+        this.year_of_issue = year_of_issue;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYear_of_issue() {
+        return year_of_issue;
+    }
+
+    public void getbike() {
+        System.out.println(getModel());
+        System.out.println(getColor());
+        System.out.println(getYear_of_issue());
     }
 }
