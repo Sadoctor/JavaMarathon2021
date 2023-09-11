ba5879a2ce282af9cdd6aa1e05bbeadfb6b3b78e
Boris Goncharuk <boris_goncharuk99@mail.ru>
2023-09-11 21:12:37 +0400

    complete day14 tasks

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,37 @@
 package day14;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File(".txt");
 
+        printSumDigits(file);
+    }
+
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String line = scanner.nextLine();
+            String[] numbers =  line.split(" ");
+
+            if (numbers.length != 10)
+                throw new IllegalArgumentException();
+
+            int sum = 0;
+            for (String number : numbers)
+                sum += Integer.parseInt(number);
+
+            System.out.println(sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
