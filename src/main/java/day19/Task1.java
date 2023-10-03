50c2b93aec797c4852121f3a07eecbffde2d9352
Boris Goncharuk <boris_goncharuk99@mail.ru>
2023-10-03 18:44:36 +0400

    complete day19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,54 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:/Users/Борис/IdeaProjects/JavaMarathon2021/src/main/resources/dushi.txt");
 
+        Scanner sc1 = new Scanner(file);
+        sc1.useDelimiter("[„<>«».,:;()?!\"\\s–]+");
+
+        Map<String, Integer> countWords = new HashMap<>();
+
+        int count = 0;
+        while(sc1.hasNext()) {                                                          // Занесение всех слов в Мар
+            countWords.put(sc1.next(), 1);
+            count++;
+        }
+        System.out.println("Колличество обработанных слов = " + count);
+        System.out.println("Колличество слов в словаре = " + countWords.size());
+
+        Scanner sc2 = new Scanner(file);
+        sc2.useDelimiter("[„<>«».,:;()?!\"\\s–]+");
+
+        while(sc2.hasNext()) {                                                          // Подсчет и запись кол-ва вхождеий каждого слова в тексте
+            String str = sc2.next();
+            if (countWords.containsKey(str))
+                countWords.replace(str, (countWords.get(str) + 1));
+        }
+
+        for (Map.Entry<String, Integer> word: countWords.entrySet()) {                  // Вывод Map в консоль
+            System.out.println(word.getKey() + " : " + word.getValue());                // Чичиков - 608
+        }
+
+        System.out.println("100 самых часто используемых слов: ");
+        for(int i = 0; i < 100; i++){                                                    // Цикл для вывода ста максимально встречающихся слов
+            String maxKey = Collections.max(countWords.entrySet(), Map.Entry.comparingByValue()).getKey();  // Ключ с максимальным значением в Map
+            int maxValue = Collections.max(countWords.entrySet(), Map.Entry.comparingByValue()).getValue(); // Максимальное значение в Map
+            System.out.println((i + 1) + ") " + maxKey + " : " + maxValue);              // Вывод элемента словаря
+            countWords.replace(maxKey, 0);                                               // Перезапись максимального значения на минимальное
+        }
+
+        sc1.close();
+        sc2.close();
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,10 +1,66 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:/Users/Борис/IdeaProjects/JavaMarathon2021/src/main/resources/taxi_cars.txt");
+
+        Scanner sc1 = new Scanner(file);
+        Map<Integer, Point> cars = new HashMap<>();
+
+        while (sc1.hasNextLine()){
+            String line = sc1.nextLine();
+            String [] car = line.split(" ");
+
+            int carKey = Integer.parseInt(car[0]);  // Есть ли необходимость вводить дополнительные переменные для лучшей читабельности кода?
+            int carX = Integer.parseInt(car[1]);
+            int carY = Integer.parseInt(car[2]);
+
+            cars.put(carKey, new Point(carX, carY));
+
+        }
+
+        sc1.close();
+
+//        for (Map.Entry<Integer, Point> car: cars.entrySet()) {
+//            System.out.println(car.getKey() + " : " + car.getValue().toString());
+//        }
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Введите 4 координаты");
+
+        System.out.print("х1: ");
+        int x1 = sc2.nextInt();
+        System.out.print("у1: ");
+        int y1 = sc2.nextInt();
+        System.out.print("х2: ");
+        int x2 = sc2.nextInt();
+        System.out.print("у2: ");
+        int y2 = sc2.nextInt();
+        
+        sc2.close();
+        
+//        System.out.println("Координаты первой точки: (" + x1 + ", " + y1 + ") \nКоординаты второй точки: (" + x2 + ", " + y2 + ")");
+
+        int sumCars = 0;
+        System.out.print("Идентификаторы машин, которые находятся внутри квадрата: ");
+        for (Map.Entry<Integer, Point> car: cars.entrySet()) {
+           int keyCar = car.getKey();
+           if((cars.get(keyCar).getX() > x1 && cars.get(keyCar).getX() < x2) && (cars.get(keyCar).getY() > y1 && cars.get(keyCar).getY() < y2)){
+               System.out.print(car.getKey() + " ");
+               sumCars++;
+           }
+        }
+//        System.out.println();
+        System.out.println("\nОбщее количество машин в квадрате: " + sumCars);
 
     }
 }
